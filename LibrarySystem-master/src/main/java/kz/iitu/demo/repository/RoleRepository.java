545107518c4a5ccd9f52cfbb494e74ab57dc44fa
eternalmgy01 <60344310+eternalmgy01@users.noreply.github.com>
2021-05-26 08:22:44 +0300

    Delete LibrarySystem-master directory

--- LibrarySystem-master/src/main/java/kz/iitu/demo/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package kz.iitu.demo.repository;
-
-import kz.iitu.demo.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
--- LibrarySystem-master/src/main/java/kz/iitu/demo/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package kz.iitu.demo.service;
-
-import kz.iitu.demo.entity.Member;
-
-public interface MemberService {
-    Member getMember(Long id);
-    void addMember(Member member);
-    int generateID();
-}
