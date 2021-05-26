6eecc2311b3fa3fdd4797d20536b679c0521fc13
eternalmgy01 <60344310+eternalmgy01@users.noreply.github.com>
2021-05-26 08:23:10 +0300

    Delete src/main directory

--- src/main/resources/db/migration/V2__insert.sql
+++
@@ -1,8 +0,0 @@
-insert into users(id, nickname, email, password, cash)
-values(1, 'alish', 'alish@gmail.com', 'alish1234', 2000);
-insert into users(id, nickname, email, password, cash)
-values(2, 'karina', 'karina@gmail.com', '1234', 10000);
-insert into users(id, nickname, email, password, cash)
-values(3, 'alibi', 'alibi@gmail.com', '0000', 5000);
-insert into users(id, nickname, email, password, cash)
-values(4, 'admin', 'admin@gmail.com', 'admin', 50000);
