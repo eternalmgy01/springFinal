e5d49e51b92cff235b65c48ac5d36ebf17d5c3ec
eternalmgy01 <60344310+eternalmgy01@users.noreply.github.com>
2021-05-26 08:20:35 +0300

    Delete EAD3_midterm-master directory

--- EAD3_midterm-master/src/main/java/org/example/midterm/repository/UserDataRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.midterm.repository;
-
-import org.example.midterm.model.UserData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDataRepository extends JpaRepository<UserData, Long> {
-}
